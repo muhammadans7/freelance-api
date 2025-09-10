19e7b90999b3e5356a7bcca20ff3561b5f00ac81
muhammadans7 <muhammadans2990@gmail.com>
2025-09-10 19:36:06 +0500

    commit for production

--- FreelanceAPI/settings.py
+++ FreelanceAPI/settings.py
@@ -95,9 +95,9 @@ DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
 
 CORS_ALLOWED_ORIGINS = os.getenv(
-    "CORS_ALLOWED_ORIGINS", "http://localhost:3000"
+    "CORS_ALLOWED_ORIGINS", "http://localhost:5173,https://freelance-frontend.onrender.com"
 ).split(",")
-CORS_ALLOW_CREDENTIALS = True
+
 
 
 STATIC_URL = "/static/"
