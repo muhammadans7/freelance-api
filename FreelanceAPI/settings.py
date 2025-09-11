05fac18a17098021f292eec6dd525662763a094a
muhammadans7 <muhammadans2990@gmail.com>
2025-09-11 14:16:57 +0500

    initial commit for prodcution

--- FreelanceAPI/settings.py
+++ FreelanceAPI/settings.py
@@ -94,10 +94,14 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
 
-CORS_ALLOWED_ORIGINS = os.getenv(
-    "CORS_ALLOWED_ORIGINS", "http://localhost:5173,https://freelance-frontend.onrender.com"
-).split(",")
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://skilvex.vercel.app",
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://skilvex.vercel.app",
+]
 
 
 STATIC_URL = "/static/"
